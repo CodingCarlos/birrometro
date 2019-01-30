523baf57efe10567a3a22bcb68ebb03c1657adc9
Carlos Hernandez <carloshdmn@gmail.com>
2019-01-30 18:07:13 +0100

    build for prod

---
+++ dist/precache-manifest.9e43336adb2ce21a5840069b0c64adb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "2a2576d4743ca066c27b",
+    "url": "js/chunk-vendors.719b9ba9.js"
+  },
+  {
+    "revision": "7bf7c020eb5ba1cbde7c",
+    "url": "js/app.b43ab0b1.js"
+  },
+  {
+    "revision": "ba8156be8b1b5bd4f19a",
+    "url": "js/about.0d02e657.js"
+  },
+  {
+    "revision": "53d2c95e38dff274686ab4dd601284fa",
+    "url": "index.html"
+  },
+  {
+    "revision": "2a2576d4743ca066c27b",
+    "url": "css/chunk-vendors.fa330473.css"
+  },
+  {
+    "revision": "7bf7c020eb5ba1cbde7c",
+    "url": "css/app.45481a4c.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.949f7b699cfe1a908fc5d6301e21adeb.js"
+  "precache-manifest.9e43336adb2ce21a5840069b0c64adb6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "birrometro"});
