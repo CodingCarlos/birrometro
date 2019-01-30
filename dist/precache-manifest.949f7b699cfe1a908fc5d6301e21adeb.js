69a0ce16d45e2efa48c37a06bb09d1737275bb19
Carlos Hernandez <carloshdmn@gmail.com>
2019-01-30 17:19:01 +0100

    Add dist (for demo)

---
+++ dist/precache-manifest.949f7b699cfe1a908fc5d6301e21adeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2a2576d4743ca066c27b",
+    "url": "/js/chunk-vendors.719b9ba9.js"
+  },
+  {
+    "revision": "b2ca842f7abb2c738c48",
+    "url": "/js/app.a887c414.js"
+  },
+  {
+    "revision": "ba8156be8b1b5bd4f19a",
+    "url": "/js/about.0d02e657.js"
+  },
+  {
+    "revision": "af05f7e09791ff07f25e93fcf5428a4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a2576d4743ca066c27b",
+    "url": "/css/chunk-vendors.fa330473.css"
+  },
+  {
+    "revision": "b2ca842f7abb2c738c48",
+    "url": "/css/app.45481a4c.css"
+  }
+];
